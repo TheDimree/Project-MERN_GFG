237da551fd4d274549a17218da9133665881bd72
Bhagesh Dimri <TheDimree@gmail.com>
2025-02-03 11:41:07 +0530

    Atlas setup is done

--- backend/src/config/Db.js
+++ backend/src/config/Db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-const DB_URL = process.env.CON_URL ?? "mongodb://localhost:27017/merndemoproject";  // * MongoDB UR, merndemoproject is the db name
+// const DB_URL = process.env.CON_URL ?? "mongodb://localhost:27017/merndemoproject";  // * MongoDB UR, merndemoproject is the db name
+
+// * Using MongoDB Atlas
+const DB_URL = process.env.CON_URL ?? "mongodb+srv://thedimri:myPassword@cluster.g217b.mongodb.net/merndemoproject?retryWrites=true&w=majority&appName=Cluster";    // merndemoproject is db name and appName is cluster name.
 
 // console.log(DB_URL)
 
